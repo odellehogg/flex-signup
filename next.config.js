2a41f67a61f179d685084f977c450a3ed9e8edbe
Odelle Hogg <odellehogg@gmail.com>
2026-01-03 00:57:04 +0000

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // No special config needed for MVP
+  // Vercel handles serverless functions automatically
+}
+
+module.exports = nextConfig
