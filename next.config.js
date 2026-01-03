f088aed3604cd240c9fad6a3002e61ed88a2b03c
Odelle Hogg <odellehogg@gmail.com>
2026-01-03 21:01:36 +0000

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // No special config needed for MVP
-  // Vercel handles serverless functions automatically
-}
-
-module.exports = nextConfig
