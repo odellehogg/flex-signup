cdff4db51bc29634a599b8b63968d10e5cdf1e5d
Odelle Hogg <odellehogg@gmail.com>
2026-03-19 10:09:32 +0000

    Update route.js

--- app/api/portal/verify-code/route.js
+++ app/api/portal/verify-code/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 // app/api/portal/verify-code/route.js
 // ============================================================================
 // PORTAL LOGIN CODE VERIFICATION
